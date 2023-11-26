46bdda915a007b719e739bf4aa0db6b2d6a9f92f
sahmed <engsameergoumaa@gmail.com>
2023-11-26 11:27:01 +0300

    4th js

---
+++ mutability.js
@@ -0,0 +1,19 @@
+const person = {
+    name: 'John',
+    age: 30,
+    country: 'US'
+  };
+
+  const clone1 = { ...person };
+
+  const clone2 = Object.assign({}, person);
+
+  const samePerson = JSON.parse(JSON.stringify(person));
+
+  person.age += 1;
+  person.country = 'FR';
+
+  console.log('Original person:', person);
+  console.log('Clone 1:', clone1);
+  console.log('Clone 2:', clone2);
+  console.log('Same Person:', samePerson);
