8eb4a5a5d35868eb37f1c923f5e82e68c9b6d1ed
sahmed <engsameergoumaa@gmail.com>
2023-11-26 13:46:04 +0300

    16th js

---
+++ is.js
@@ -0,0 +1,45 @@
+const is = {
+    num: function(value) {
+      return typeof value === 'number' && !isNaN(value);
+    },
+
+    nan: function(value) {
+      return typeof value === 'number' && isNaN(value);
+    },
+
+    str: function(value) {
+      return typeof value === 'string';
+    },
+
+    bool: function(value) {
+      return typeof value === 'boolean';
+    },
+
+    undef: function(value) {
+      return typeof value === 'undefined';
+    },
+
+    def: function(value) {
+      return typeof value !== 'undefined';
+    },
+
+    arr: function(value) {
+      return Array.isArray(value);
+    },
+
+    obj: function(value) {
+      return value !== null && typeof value === 'object' && !Array.isArray(value);
+    },
+
+    fun: function(value) {
+      return typeof value === 'function';
+    },
+
+    truthy: function(value) {
+      return Boolean(value);
+    },
+
+    falsy: function(value) {
+      return !Boolean(value);
+    }
+  };
