69a2df00579214d666e2f176d4d9d8b796df5581
sahmed <engsameergoumaa@gmail.com>
2023-11-26 11:48:26 +0300

    4th js

--- mutability.js
+++ mutability.js
@@ -1,6 +1,7 @@
   const clone1 = { ...person };
   const clone2 = Object.assign({}, person);
-
+  Object.freeze(clone1);
+  Object.freeze(clone2);
   var samePerson = structuredClone(person);
 
   person.age += 1;
