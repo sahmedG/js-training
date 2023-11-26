81cb1906bc3b9d52b2ef9ed970782e9bc66c5ce7
sahmed <engsameergoumaa@gmail.com>
2023-11-26 13:40:05 +0300

    15th js

---
+++ min-max.js
@@ -0,0 +1,16 @@
+function max(x, y) {
+    if (x > y) {
+        return x
+    } else {
+        return y
+    }
+}
+
+function min(x, y) {
+    if (x < y) {
+        return x
+    } else {
+        return y
+    }
+}
+
