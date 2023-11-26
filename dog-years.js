89c8114bab6a86c0add02c8c46a97528ce3b81a0
sahmed <engsameergoumaa@gmail.com>
2023-11-26 12:49:36 +0300

    9th js

---
+++ dog-years.js
@@ -0,0 +1,30 @@
+function dogYears(planet, ageInSeconds) {
+    const earthYearInSeconds = 31557600; // Earth year in seconds
+
+    // Mapping of orbital periods for each planet relative to Earth
+    const orbitalPeriods = {
+      earth: 1.0,
+      mercury: 0.2408467,
+      venus: 0.61519726,
+      mars: 1.8808158,
+      jupiter: 11.862615,
+      saturn: 29.447498,
+      uranus: 84.016846,
+      neptune: 164.79132,
+    };
+
+    // Calculate dog years on the specified planet
+    const dogYearsOnPlanet = ageInSeconds / (orbitalPeriods[planet] * earthYearInSeconds);
+
+    // Round the result to two decimal places
+    const roundedDogYears = Math.round(dogYearsOnPlanet * 100) / 100;
+
+    return roundedDogYears;
+  }
+
+  // Example usage:
+  const ageInSeconds = 1000000000;
+  const dogAgeOnEarth = dogYears('earth', ageInSeconds);
+  console.log(`Dog age on Earth: ${dogAgeOnEarth} years`);
+
+  // Output: Dog age on Earth: 31.69 years
