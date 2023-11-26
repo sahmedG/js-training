20beba4b52a4711451294f258666e27740bd79ec
sahmed <engsameergoumaa@gmail.com>
2023-11-26 12:41:50 +0300

    9th js

--- change.js
+++ change.js
@@ -1,21 +1,8 @@
 function get(sourceObject, key) {
     return sourceObject[key];
-  }
+}
 
-  function set(sourceObject, key, value) {
+function set(sourceObject, key, value) {
     sourceObject[key] = value;
     return value;
-  }
-
-// const sourceObject = {
-//     num: 42,
-//     bool: true,
-//     str: 'some text',
-//     log: console.log,
-//   }
-
-//   let ageValue = get(sourceObject, 'num');
-//   console.log(ageValue);
-
-//   let updatedAge = set(sourceObject, 'num', 31);
-//   console.log(sourceObject);
+}
