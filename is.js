0fbf9a62736671fd3bc2ff72d5a1224915e188f2
sahmed <engsameergoumaa@gmail.com>
2023-11-26 13:51:30 +0300

    16th js

--- is.js
+++ is.js
@@ -1,45 +1,43 @@
-const is = {
-    num: function(value) {
-      return typeof value === 'number' && !isNaN(value);
-    },
+is.num = function (value) {
+    return typeof value === 'number' && !isNaN(value);
+};
 
-    nan: function(value) {
-      return typeof value === 'number' && isNaN(value);
-    },
+is.nan = function (value) {
+    return typeof value === 'number' && isNaN(value);
+};
 
-    str: function(value) {
-      return typeof value === 'string';
-    },
+is.str = function (value) {
+    return typeof value === 'string';
+};
 
-    bool: function(value) {
-      return typeof value === 'boolean';
-    },
+is.bool = function (value) {
+    return typeof value === 'boolean';
+};
 
-    undef: function(value) {
-      return typeof value === 'undefined';
-    },
+is.undef = function (value) {
+    return typeof value === 'undefined';
+};
 
-    def: function(value) {
-      return typeof value !== 'undefined';
-    },
+is.def = function (value) {
+    return typeof value !== 'undefined';
+};
 
-    arr: function(value) {
-      return Array.isArray(value);
-    },
+is.arr = function (value) {
+    return Array.isArray(value);
+};
 
-    obj: function(value) {
-      return value !== null && typeof value === 'object' && !Array.isArray(value);
-    },
+is.obj = function (value) {
+    return value !== null && typeof value === 'object' && !Array.isArray(value);
+};
 
-    fun: function(value) {
-      return typeof value === 'function';
-    },
+is.fun = function (value) {
+    return typeof value === 'function';
+};
 
-    truthy: function(value) {
-      return Boolean(value);
-    },
+is.truthy = function (value) {
+    return Boolean(value);
+};
 
-    falsy: function(value) {
-      return !Boolean(value);
-    }
-  };
+is.falsy = function (value) {
+    return !Boolean(value);
+};
