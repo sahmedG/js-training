ef634838207d38a99f3f862e6bb6afe6f2fbbb87
sahmed <engsameergoumaa@gmail.com>
2023-11-28 17:33:31 +0300

    loop6

--- cut-corners.js
+++ cut-corners.js
@@ -1,27 +1,47 @@
 function round(num) {
-    const absNum = Math.abs(num);
-    const tens = Math.pow(10, Math.floor(Math.log10(absNum)));
-    const rounded = Math.floor(absNum / tens) * tens;
+    const fractionalPart = customModulus(num,1);
+    const isPositive = num >= 0;
+    const multiplier = isPositive ? 1 : -1;
   
-    return num < 0 ? -rounded : rounded;
+    if (fractionalPart >= 0.5) {
+      return num + multiplier;
+    } else if (fractionalPart <= -0.5) {
+      return num - multiplier;
+    } else {
+      return num;
+    }
   }
   
   function ceil(num) {
-    const absNum = Math.abs(num);
-    const tens = Math.pow(10, Math.floor(Math.log10(absNum)));
-    const ceiled = Math.ceil(absNum / tens) * tens;
+    const isPositive = num >= 0;
+    const multiplier = isPositive ? 1 : -1;
   
-    return num < 0 ? -ceiled : ceiled;
+    while (num % 1 !== 0) {
+      num += multiplier * 0.1;
+    }
+  
+    return num;
   }
   
   function floor(num) {
-    const absNum = Math.abs(num);
-    const tens = Math.pow(10, Math.floor(Math.log10(absNum)));
-    const floored = Math.floor(absNum / tens) * tens;
+    const isPositive = num >= 0;
+    const multiplier = isPositive ? -1 : 1;
   
-    return num < 0 ? -floored : floored;
+    while (customModulus(num,1) !== 0) {
+      num += multiplier * 0.1;
+    }
+  
+    return num;
   }
   
   function trunc(num) {
-    return Math.abs(num) === Infinity ? num : num >= 0 ? Math.floor(num) : Math.ceil(num);
+    return num > 0 ? floor(num) : ceil(num);
+  }
+
+  function customModulus(num, divisor) {
+    while (num >= divisor) {
+      num -= divisor;
+    }
+  
+    return num;
   }
