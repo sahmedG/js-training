3e1268b9439d96c8e5e8c152761ea8aae2fc2bc4
sahmed <engsameergoumaa@gmail.com>
2023-11-28 17:10:14 +0300

    loop6

--- cut-corners.js
+++ cut-corners.js
@@ -1,16 +1,17 @@
 function round(num) {
-    const decimalPart = num - parseInt(num);
-    return decimalPart >= 0.5 ? (num < 0 ? parseInt(num) : parseInt(num) + 1) : parseInt(num);
+    const decimalPart = num - (num | 0);
+    return decimalPart >= 0.5 ? (num < 0 ? (num | 0) - 1 : (num | 0) + 1) : num | 0;
   }
   
   function ceil(num) {
-    return num >= 0 ? (num === parseInt(num) ? num : parseInt(num) + 1) : parseInt(num);
+    return num >= 0 ? (num === (num | 0) ? num : (num | 0) + 1) : num | 0;
   }
   
   function floor(num) {
-    return num >= 0 ? parseInt(num) : (num === parseInt(num) ? num : parseInt(num) - 1);
+    return num >= 0 ? num | 0 : (num === (num | 0) ? num : (num | 0) - 1);
   }
   
   function trunc(num) {
-    return num >= 0 ? (num === parseInt(num) ? num : parseInt(num)) : (num === parseInt(num) ? num : parseInt(num) - 1);
+    return num >= 0 ? num | 0 : (num === (num | 0) ? num : (num | 0) - 1);
   }
+  
