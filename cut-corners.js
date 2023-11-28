2714a9dc2ee195013ea77c6e7c8e2863e08e127d
sahmed <engsameergoumaa@gmail.com>
2023-11-28 17:45:45 +0300

    loop6

--- cut-corners.js
+++ cut-corners.js
@@ -1,47 +1,86 @@
-function round(num) {
-    const fractionalPart = customModulus(num,1);
-    const isPositive = num >= 0;
-    const multiplier = isPositive ? 1 : -1;
-  
-    if (fractionalPart >= 0.5) {
-      return num + multiplier;
-    } else if (fractionalPart <= -0.5) {
-      return num - multiplier;
+function round(int) {
+    let neg = false;
+    if (int < 0) {
+        neg = true;
+        int = -int;
+    }
+    let counter = 0;
+    while (!(int < 1 && int > -1)) {
+        int -= 1;
+        counter++;
+    }
+    if (int < 0.5) {
+        if (neg) {
+            return -counter;
+        } else {
+            return counter;
+        }
     } else {
-      return num;
+        if (neg) {
+            return -counter - 1;
+        } else {
+            return counter + 1;
+        }
     }
-  }
-  
-  function ceil(num) {
-    const isPositive = num >= 0;
-    const multiplier = isPositive ? 1 : -1;
-  
-    while (customModulus(num,1) !== 0) {
-      num += multiplier * 0.1;
-    }
-  
-    return num;
-  }
-  
-  function floor(num) {
-    const isPositive = num >= 0;
-    const multiplier = isPositive ? -1 : 1;
-  
-    while (customModulus(num,1) !== 0) {
-      num += multiplier * 0.1;
-    }
-  
-    return num;
-  }
-  
-  function trunc(num) {
-    return num > 0 ? floor(num) : ceil(num);
-  }
+}
 
-  function customModulus(num, divisor) {
-    while (num >= divisor) {
-      num -= divisor;
+function floor(int) {
+    let neg = false;
+    if (int < 0) {
+        neg = true;
+        int = -int;
     }
-  
-    return num;
-  }
+    let intCopy = int;
+    let counter = 0;
+    while (!(intCopy < 1 && intCopy > -1)) {
+        intCopy -= 1;
+        counter++;
+    }
+    if (neg) {
+        return -counter - 1;
+    } else {
+        return counter;
+    }
+}
+
+function ceil(int) {
+    if (!int) return 0;
+    let neg = false;
+    if (int < 0) {
+        neg = true;
+        int = -int;
+    }
+    let intCopy = int;
+    let counter = 0;
+    while (!(intCopy < 1 && intCopy >= 0)) {
+        intCopy -= 1;
+        counter++;
+    }
+    if (neg) {
+        return -counter;
+    } else {
+        return counter + 1;
+    }
+}
+
+function trunc(int) {
+    let counter = 0;
+    if (int > 0xfffffffff) {
+        int -= 0xfffffffff;
+        counter += 0xfffffffff;
+    }
+    let neg = false;
+    if (int < 0) {
+        neg = true;
+        int = -int;
+    }
+    let intCopy = int;
+    while (!(intCopy < 1 && intCopy > -1)) {
+        intCopy -= 1;
+        counter++;
+    }
+    if (neg) {
+        return -counter;
+    }
+    return counter;
+}
