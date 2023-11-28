ca41d15cb3ec782762af8ab47b0373c41d6efbd2
sahmed <engsameergoumaa@gmail.com>
2023-11-28 13:36:49 +0300

    loop4

--- slicer.js
+++ slicer.js
@@ -3,7 +3,5 @@ function slice(input, start, end) {
       return input.slice(start, end);
     } else if (typeof input === 'string') {
       return input.slice(start, end);
-    } else {
-      throw new Error('Invalid input. The input must be an array or a string.');
     }
   }
