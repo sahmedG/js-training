10b0a8118ec1d70d7a13ef141734a815d763d101
sahmed <engsameergoumaa@gmail.com>
2023-11-28 13:47:22 +0300

    loop4

--- slicer.js
+++ slicer.js
@@ -1,4 +1,24 @@
 function slice(input, start, end) {
-    const inputString = String(input);
-    return inputString.slice(start, end);
-}
+    if (Array.isArray(input) || typeof input === 'string') {
+      const inputLength = input.length;
+      let sliced = '';
+  
+      if (start < 0) {
+        start = Math.max(0, start + inputLength);
+      }
+  
+      if (end < 0) {
+        end = Math.max(0, end + inputLength);
+      }
+  
+      end = Math.min(end, inputLength);
+  
+      for (let i = start; i < end; i++) {
+        sliced += input[i];
+      }
+  
+      return typeof input === 'string' ? sliced : sliced.split('');
+    } else {
+      throw new Error('Invalid input. The input must be an array or a string.');
+    }
+  }
