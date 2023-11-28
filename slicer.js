12f56f132c108783ff217240be3479fc2ad24713
sahmed <engsameergoumaa@gmail.com>
2023-11-28 13:40:09 +0300

    loop4

--- slicer.js
+++ slicer.js
@@ -1,7 +1,8 @@
 function slice(input, start, end) {
-    if (Array.isArray(input)) {
-      return input.slice(start, end);
-    } else if (typeof input === 'string') {
-      return input.slice(start, end);
+    if (Array.isArray(input) || typeof input === 'string') {
+        const inputString = String(input);
+        return inputString.slice(start, end);
+    } else {
+      throw new Error('Invalid input. The input must be an array or a string.');
     }
   }
