267d350f40c57f0c59de67631d6e110c3eab048f
sahmed <engsameergoumaa@gmail.com>
2023-11-28 19:38:53 +0300

    loop16

--- find-expression.js
+++ find-expression.js
@@ -3,19 +3,13 @@ function findExpression(target) {
       if (current === target) {
         return expression;
       } else if (current > target) {
-        return undefined;  // If the target cannot be reached
+        return undefined; 
       } else {
-        // Try both options: add 4 and multiply by 2
         const add4Result = search(add4(current), expression + `+4`);
         const mul2Result = search(mul2(current), expression + `*2`);
-        
-        // Return the first successful path or undefined if both paths fail
         return add4Result || mul2Result;
       }
     }
-  
-    // Start the search from 1
     const result = search(1, '');
-    
     return result;
   }
