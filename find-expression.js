9fa4229119480eb1ec2cfe8c95e10c161a1930e9
sahmed <engsameergoumaa@gmail.com>
2023-11-28 19:57:27 +0300

    loop16

--- find-expression.js
+++ find-expression.js
@@ -1,17 +1,17 @@
-function findExpression(num) {
+function findExpression(number) {
     for (let i = 0; i < 100000; i++) {
-        let cpy = 1;
-        let seq = "1";
-        while (cpy <= num) {
-            if (cpy === num) {
-                return seq;
+        let copy = 1;
+        let sequence = "1";
+        while (copy <= number) {
+            if (copy === number) {
+                return sequence;
             }
             if (Math.random() < 0.4 + 0.1) {
-                cpy += 4;
-                seq += " " + add4;
+                copy += 4;
+                sequence += " " + add4;
             } else {
-                cpy *= 2;
-                seq += " " + mul2;
+                copy *= 2;
+                sequence += " " + mul2;
             }
         }
     }
