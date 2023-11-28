19f8ad4e2f2d841d0e1e3855aba3cc916ae0f34a
sahmed <engsameergoumaa@gmail.com>
2023-11-28 19:02:56 +0300

    loop13

--- triangle.js
+++ triangle.js
@@ -1,9 +1,8 @@
 function triangle(character, height) {
     let result = '';
   
-    for (let i = 1; i <= height; i++) {
+    for (let i = 1; i < height; i++) {
       result += character.repeat(i) + '\n';
     }
-  
     return result;
   }
