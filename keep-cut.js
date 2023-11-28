089ec8daae88162e81f7168909a69de730cc9425
sahmed <engsameergoumaa@gmail.com>
2023-11-28 14:04:08 +0300

    loop5

---
+++ keep-cut.js
@@ -0,0 +1,23 @@
+function cutFirst(input) {
+    return input.slice(2);
+  }
+  
+  function cutLast(input) {
+    return input.slice(0, -2);
+  }
+  
+  function cutFirstLast(input) {
+    return input.slice(2, -2);
+  }
+  
+  function keepFirst(input) {
+    return input.slice(0, 2);
+  }
+  
+  function keepLast(input) {
+    return input.slice(-2);
+  }
+  
+  function keepFirstLast(input) {
+    return input.slice(0, 2) + input.slice(-2);
+  }
