5dafdb5fca07862c4a588f1c0f1677a1e2d151d3
sahmed <engsameergoumaa@gmail.com>
2023-11-30 12:17:32 +0300

    find4

--- same-amount.js
+++ same-amount.js
@@ -1,4 +1,6 @@
 function sameAmount(inputString,regex1,regex2) {
+    regex1 = new RegExp(regex1,"g");
+    regex2 = new RegExp(regex2,"g");
     let result1 = inputString.match(regex1);
     let result2 = inputString.match(regex2);
     return result1 !== null && result2 !== null && result1.length === result2.length;
