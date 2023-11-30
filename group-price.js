46960e610e85f068e43431e07b79eb815a20af55
sahmed <engsameergoumaa@gmail.com>
2023-11-30 12:58:39 +0300

    find7

--- group-price.js
+++ group-price.js
@@ -1,10 +1,11 @@
 function groupPrice(str) {
-    let prices = str.match(/(([A-Z]{3})|\$)([0-9]+\.[0-9]+)/g);
-    let res = [];
-    if (prices === null) return res;
-    prices.forEach((price) => {
-        let [fullPrice, dollars, cents] = price.match(/[0-9]+/g);
-        res.push([fullPrice, dollars, cents]);
+    let matches = str.match(/(([A-Z]{3})|\$)([0-9]+\.[0-9]+)/g);
+    let result = [];
+    if (matches === null) return result;
+    matches.forEach((price, i) => {
+        result.push([price]);
+        result[i].push(price.match(/[0-9]+/g)[0]);
+        result[i].push(price.match(/[0-9]+/g)[1]);
     });
-    return res;
+    return result;
 }
