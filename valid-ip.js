df1765dca88a183a58657780204080d9ce6ef69c
sahmed <engsameergoumaa@gmail.com>
2023-11-30 13:04:58 +0300

    find8

--- valid-ip.js
+++ valid-ip.js
@@ -1,5 +1,5 @@
 function findIP(inputString){
     const ipregex = /\b(?:\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(?:\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(?:\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(?:\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\b(?::\d{1,5})?/g;
-    const matches = inputString.matches(ipregex) || [];
+    const matches = inputString.match(ipregex) || [];
     return matches;
 }
