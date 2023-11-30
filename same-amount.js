1d8cfde9655b0caeced89ab8300680dfe28f0d4a
sahmed <engsameergoumaa@gmail.com>
2023-11-30 12:16:10 +0300

    find4

---
+++ same-amount.js
@@ -0,0 +1,5 @@
+function sameAmount(inputString,regex1,regex2) {
+    let result1 = inputString.match(regex1);
+    let result2 = inputString.match(regex2);
+    return result1 !== null && result2 !== null && result1.length === result2.length
+}
