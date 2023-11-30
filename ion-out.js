c5018e0a09cf135ca53491a6024c3c3f4969fdd0
sahmed <engsameergoumaa@gmail.com>
2023-11-30 12:24:47 +0300

    find5

---
+++ ion-out.js
@@ -0,0 +1,9 @@
+function ionOut(inputString) {
+    const regex1 = /tion/g;
+    let input_array = inputString.split(" ");
+    let result_array =[];
+    input_array.forEach((word) => {
+        word.match(regex1) ? result_array.push(word.replace(/[.,?!]g/,"")/slice(0,-3)) : null;
+    });
+    return result_array
+}
