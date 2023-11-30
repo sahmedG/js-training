133dd305b4ad0c215a74c43b2bf12a65c0828a69
sahmed <engsameergoumaa@gmail.com>
2023-11-30 11:53:45 +0300

    find2

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,6 +1,6 @@
 function letterSpaceNumber(inputString) {
     const result = [];
-    const regex = /[a-zA-Z] \d(?![a-zA-Z])/g;
+    const regex = /[a-zA-Z] \b\d\b/g;
     const matches = inputString.match(regex);
     if (matches) {
       matches.forEach(match => {
