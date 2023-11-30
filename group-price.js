5a2109395db150dc6eb8ea466f5e372af295096c
sahmed <engsameergoumaa@gmail.com>
2023-11-30 12:42:37 +0300

    find7

---
+++ group-price.js
@@ -0,0 +1,14 @@
+function groupPrice(inputString) {
+    const priceRegex = /([A-Za-z]+)(\d+\.\d{2})/;
+    const match = inputString.match(priceRegex);
+
+    if(!match){
+        return [];
+    }
+
+    const [,currency,fullprice] = match;
+    const [dollars,cents] = fullprice.split('.');
+    const pricebreak = [[`${currency}${fullprice}`,dollars,cents]];
+
+    return pricebreak
+}
