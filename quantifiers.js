024b4802f99c44db18f38a404c673fd7bd1b7d62
sahmed <engsameergoumaa@gmail.com>
2023-12-04 11:42:45 +0300

    mall-me-maybe-9

---
+++ quantifiers.js
@@ -0,0 +1,12 @@
+function every(array,func){
+    for (let i =0;i<array.length;i++){
+        if(!func(array[i])) return false;
+    }
+    return true;
+}
+function some(array,func){
+    return true;
+}
+function some(array,func){
+    return true;
+}
