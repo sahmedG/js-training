7555189acdeb130c0e17cc53b4de9f77eab1cd68
sahmed <engsameergoumaa@gmail.com>
2023-12-04 12:58:56 +0300

    mall-me-maybe-12

--- flow.js
+++ flow.js
@@ -1,5 +1,8 @@
 function flow(funcs){
-    return function(...args) {
+    return function (...args) {
+        if (args.length > 1) {
+            args = [arr[0](...args)];
+        }
         return funcs.reduce((acc, fn) => fn(acc), args[0]);
-    };
+    };};
 }
