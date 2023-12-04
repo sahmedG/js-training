950df6d665ec9c19c03c7fbdd36793fe106d7f82
sahmed <engsameergoumaa@gmail.com>
2023-12-04 11:22:13 +0300

    mall-me-maybe-1

--- using-reduce.js
+++ using-reduce.js
@@ -1,15 +1,32 @@
-function adder(arr) {
-    return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-  }
-  
-  function sumOrMul(arr, initial = 0) {
-    return arr.reduce((accumulator, currentValue) => {
-      return currentValue % 2 === 0 ? accumulator * currentValue : accumulator + currentValue;
-    }, initial);
-  }
-  
-  function funcExec(arr, initial) {
-    return arr.reduce((accumulator, currentFunction) => {
-      return currentFunction(accumulator);
-    }, initial);
-  }
+function adder(arr, value) {
+    return arr.reduce(
+        (acc, item) => acc + item,
+        value === undefined ? 0 : value
+    );
+}
+
+function sumOrMul(arr, value) {
+    return arr.reduce(
+        (acc, item) => {
+            if (item % 2 === 0) {
+                return acc * item;
+            } else {
+                return acc + item;
+            }
+        },
+        value === undefined ? 0 : value
+    );
+}
+
+function funcExec(arr, value) {
+    return arr.reduce(
+        (acc, item) => {
+            if (typeof item === "function") {
+                return item(acc, value);
+            } else {
+                return acc;
+            }
+        },
+        value === undefined ? 0 : value
+    );
+}
