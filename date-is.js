49389de6077d1635260924706cfb1d007c47ea72
sahmed <engsameergoumaa@gmail.com>
2023-12-04 07:59:57 +0300

    time2

--- date-is.js
+++ date-is.js
@@ -1,23 +1,43 @@
+function isValid(date) {
+  if (new Date(date).toString() === "Invalid Date") {
+      return false;
+  }
+  if (!(date instanceof Date) && typeof date !== "number") {
+      return false;
+  }
+  return true;
+}
+
 function isAfter(date1, date2) {
-  return date1.getTime() > date2.getTime();
+  if (date1 > date2) {
+      return true;
+  }
+  return false;
 }
 
 function isBefore(date1, date2) {
-  return date1.getTime() < date2.getTime();
+  if (date1 < date2) {
+      return true;
+  }
+  return false;
 }
 
 function isFuture(date) {
   if (!isValid(date)) {
-    return false;
+      return false;
   }
-  const presentDate = new Date();
-  return isAfter(date, presentDate);
+  if (new Date(date).getTime() > Date.now()) {
+      return true;
+  }
+  return false;
 }
 
 function isPast(date) {
   if (!isValid(date)) {
-    return false;
+      return false;
   }
-  const presentDate = new Date();
-  return isBefore(date, presentDate);
+  if (new Date(date).getTime() < Date.now()) {
+      return true;
+  }
+  return false;
 }
