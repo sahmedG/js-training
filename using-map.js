0798aa7af792b84bc78020e63445d897964be682
sahmed <engsameergoumaa@gmail.com>
2023-12-04 10:34:15 +0300

    call-me-maybe-1

---
+++ using-map.js
@@ -0,0 +1,20 @@
+function citiesOnly(arr) {
+    return arr.map(obj => obj.city.trim());
+  }
+
+  function upperCasingStates(arr) {
+    return arr.map(state => state.replace(/\b\w/g, char => char.toUpperCase()));
+  }
+
+  function fahrenheitToCelsius(arr) {
+    const fToC = fahrenheit => `${Math.floor((parseInt(fahrenheit) - 32) * 5 / 9)}°C`;
+    return arr.map(temp => fToC(temp.replace(/\D/g, '')));
+  }
+
+  function trimTemp(arr) {
+    return arr.map(obj => ({ city: obj.city, temperature: obj.temperature.trim() }));
+  }
+
+  function tempForecasts(arr) {
+    return arr.map(obj => `${fahrenheitToCelsius([obj.temperature])[0]} in ${obj.city}, ${obj.state}`);
+  }
