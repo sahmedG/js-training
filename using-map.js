bd09ddb7dd77340870525e7dd0e9b13facbf9e32
sahmed <engsameergoumaa@gmail.com>
2023-12-04 10:55:46 +0300

    mall-me-maybe-1

--- using-map.js
+++ using-map.js
@@ -22,5 +22,5 @@ function citiesOnly(arr) {
   }
 
   function tempForecasts(arr) {
-    return arr.map(obj => `${fahrenheitToCelsius([obj.temperature])[0]} in ${obj.city}, ${obj.state}`);
+    return arr.map(obj => `${fahrenheitToCelsius([obj.temperature]).replaceAll("°C","°Celsius")[0]} in ${obj.city}, ${obj.state}`);
   }
