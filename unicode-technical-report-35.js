292ea61833a186fbcf5c388eac325460df60a706
sahmed <engsameergoumaa@gmail.com>
2023-12-04 09:18:40 +0300

    finaly

--- unicode-technical-report-35.js
+++ unicode-technical-report-35.js
@@ -4,38 +4,18 @@ Date.prototype.year = Date.prototype.getFullYear;
 Date.prototype.date = Date.prototype.getDate;
 Date.prototype.hours = Date.prototype.getHours;
 
-function format(date, formatString) {
-  const d = new Date(date);
-  const lM = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const sM = lM.map((m) => m.slice(0, 3));
-  const lD = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const sD = lD.map((d) => d.slice(0, 3));
-
-  const replaceToken = (token, value) => {
-    formatString = formatString.replace(new RegExp(token, 'g'), value);
-  };
-  replaceToken('dd', ("0" + d.date()).slice(-2));
+function format(date, format) {
+    const d = new Date(date);
+    const lM = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const sM = lM.map((m) => m.slice(0, 3));
+    const lD = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const sD = lD.map((d) => d.slice(0, 3));
+  
+      const replaceToken = (token, value) => {
+      format = format.replace(new RegExp(token, 'g'), value);
+    };
+  
+   replaceToken('dd', ("0" + d.date()).slice(-2));
   replaceToken('d', d.date());
 
   replaceToken('HH', ("0" + d.hours()).slice(-2));
@@ -58,14 +38,14 @@ function format(date, formatString) {
 
   replaceToken('a', d.hours() < 12 ? "AM" : "PM");
 
-  replaceToken('MM', (d.month() + 1).toString().padStart(2, "0"));
-  replaceToken('M', d.month() + 1);
-  replaceToken('MMMM', lM[d.month()]);
-  replaceToken('MMM', sM[d.month()].slice(0, 3));
+  // Adjusted handling for month strings
+  format = format.replace(/(?<!M)MM(?!M)/g,(d.month() + 1).toString().length < 10? "0" + (d.month() + 1): d.month() + 1);
+    format = format.replace(/(?<!(M|P|A))M(?!M)/g, d.month() + 1);
+    format = format.replace(/MMMM/g, lM[d.month()]);
+    format = format.replace(/MMM/g, sM[d.month()].slice(0, 3));
 
   replaceToken('EEEE', lD[d.getDay()]);
   replaceToken('E', sD[d.getDay()].slice(0, 3));
 
-  return formatString;
+  return format;
 }
-
