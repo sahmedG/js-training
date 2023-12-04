3b40c6d6b142bf05f82ca98be354ea167aa5bfdb
sahmed <engsameergoumaa@gmail.com>
2023-12-04 11:24:58 +0300

    mall-me-maybe-5

--- for-each.js
+++ for-each.js
@@ -1,5 +1,5 @@
-function forEach(array,action){
-    for(let i=0;i<array.length;i++){
-        action(array[i],i,array);
-    };
-}
+function forEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+      callback(arr[i], i, arr);
+    }
+  }
