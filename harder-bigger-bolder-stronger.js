1806bdd61233e344d04cfd799eab9bced3acaf03
sahmed <engsameergoumaa@gmail.com>
2023-12-07 17:41:19 +0300

    dom-3

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,22 +1,14 @@
-function generateLetters() {
-    const lettersContainer = document.getElementById('letters-container');
-
+export function generateLetters() {
     for (let i = 0; i < 120; i++) {
-        const letter = document.createElement('div');
-        letter.classList.add('letter');
-
-        // Random uppercase letter
+        let letter = document.createElement("div");
+        letter.style.fontSize = `${11 + i}px`;
         const randomLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
         letter.textContent = randomLetter;
-
-        // Calculate style properties based on index
         const fontSize = 11 + (i / 119) * (130 - 11);
         const fontWeight = i < 40 ? 300 : (i < 80 ? 400 : 600);
-
         letter.style.fontSize = `${fontSize}px`;
         letter.style.fontWeight = fontWeight;
 
-        lettersContainer.appendChild(letter);
+        document.getElementsByTagName("body")[0].appendChild(letter);
     }
 }
-export {generateLetters};
