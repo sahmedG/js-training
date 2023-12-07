0991b0d5423f8c2fd704ddc591301da5f595f7b6
sahmed <engsameergoumaa@gmail.com>
2023-12-07 17:29:44 +0300

    dom-2

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,49 +1,28 @@
-function build(numberOfBricks) {
-    let brickCounter = 0;
-  
-    const interval = setInterval(() => {
-      brickCounter++;
-  
-      const newBrick = document.createElement('div');
-      newBrick.id = `brick-${brickCounter}`;
-  
-      if (brickCounter % 3 === 2) {
-        newBrick.dataset.foundation = true;
-      }
-  
-      document.getElementById('tower').appendChild(newBrick);
-  
-      if (brickCounter === numberOfBricks) {
-        clearInterval(interval);
-      }
+function build(arr) {
+    let body = document.getElementsByTagName("body")[0];
+    let bricks = 1;
+    let interval = setInterval(() => {
+        let brick = document.createElement("div");
+        brick.setAttribute("id", "brick-" + bricks);
+        bricks % 3 === 2 ? (brick.dataset.foundation = true) : null;
+        body.appendChild(brick);
+        bricks++;
+        if (bricks > arr) {
+            clearInterval(interval);
+        }
     }, 100);
-  }
-  
-  function repair(...brickIds) {
-    for (const brickId of brickIds) {
-      const brick = document.getElementById(brickId);
-  
-      if (brick.dataset.foundation === 'true') {
-        brick.dataset.repaired = 'in progress';
-      } else {
-        brick.dataset.repaired = true;
-      }
-    }
-  }
-  
-  function destroy() {
-    const bricks = document.querySelectorAll('#tower div');
-    const lastBrick = bricks[bricks.length - 1];
-    lastBrick.parentNode.removeChild(lastBrick);
-  }
-  
-  // Add click event listeners to the emojis
-  document.getElementById('repair-emoji').addEventListener('click', () => {
-    // Get the selected brick IDs
-    const selectedBricks = document.querySelectorAll('.selected');
-    const brickIds = [...selectedBricks].map(brick => brick.id);
-  
-    repair(...brickIds);
-  });
-  
-  document.getElementById('destroy-emoji').addEventListener('click', destroy);
+}
+
+function repair(...ids) {
+    ids.forEach((ids) => {
+        let brick = getElementsById(id);
+        brick.getAttribute("foundation") ? (brick.dataset.repaired = "in progress") : (brick.dataset.repaired = true);
+    });
+}
+
+function destroy(){
+    let bricks = document.getElementsByTagName("div");
+    bricks[bricks.length - 1].remove();
+}
+
+export {build,repair,destroy};
