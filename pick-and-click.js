4aedbfaffc2d6817f60ecc332f4253191a416bd4
sahmed <engsameergoumaa@gmail.com>
2023-12-07 18:31:01 +0300

    dom-8

--- pick-and-click.js
+++ pick-and-click.js
@@ -1,31 +1,81 @@
-export function pick(event) {
-    const hue = Math.round(event.clientX / window.innerWidth * 360);
+document.addEventListener("mousemove", (e) => {
+    pick(e);
+});
 
-    const luminosity = Math.round((window.innerHeight - event.clientY) / window.innerHeight * 100);
+document.addEventListener("click", (e) => {
+    pick(e);
+    copyHSL();
+});
 
-    document.body.style.backgroundColor = `hsl(${hue}, 50%, ${luminosity}%)`;
+const hslDiv = document.createElement("div");
+hslDiv.classList.add("hsl");
+document.body.appendChild(hslDiv);
 
-    const hslElement = document.querySelector('.hsl');
-    hslElement.textContent = `hsl(${hue}, 50%, ${luminosity}%)`;
+const hueDiv = document.createElement("div");
+hueDiv.classList.add("hue", "text");
+document.body.appendChild(hueDiv);
 
-    const hueElement = document.querySelector('.hue');
-    hueElement.textContent = `Hue: ${hue}`;
-  
-    const luminosityElement = document.querySelector('.luminosity');
-    luminosityElement.textContent = `Luminosity: ${luminosity}`;
-  
-    const axisX = document.getElementById('axisX');
-    axisX.setAttribute('x1', 0);
-    axisX.setAttribute('x2', window.innerWidth);
-  
-    const axisY = document.getElementById('axisY');
-    axisY.setAttribute('y1', 0);
-    axisY.setAttribute('y2', window.innerHeight);
+const luminosityDiv = document.createElement("div");
+luminosityDiv.classList.add("luminosity", "text");
+document.body.appendChild(luminosityDiv);
 
-    document.body.addEventListener('click', () => {
-      navigator.clipboard.writeText(hslElement.textContent);
-    });
-  }
+const svgns = "http://www.w3.org/2000/svg";
+const svg = document.createElement("svg");
+svg.id = "svg";
+svg.setAttribute("width", "100%");
+svg.setAttribute("height", "100%");
+svg.setAttribute("viewBox", "0 0 100% 100%");
+svg.setAttribute("preserveAspectRatio", "none");
 
-  document.body.addEventListener('mousemove', pick);
-  
+const axisX = document.createElementNS(svgns, "line");
+axisX.id = "axisX";
+axisX.setAttribute("x1", "0");
+axisX.setAttribute("y1", "0");
+axisX.setAttribute("x2", "0");
+axisX.setAttribute("y2", "100%");
+axisX.setAttribute("stroke", "red");
+axisX.setAttribute("stroke-width", "3");
+svg.appendChild(axisX);
+
+const axisY = document.createElementNS(svgns, "line");
+axisY.id = "axisY";
+axisY.setAttribute("x1", "0");
+axisY.setAttribute("y1", "0");
+axisY.setAttribute("x2", "100%");
+axisY.setAttribute("y2", "0");
+axisY.setAttribute("stroke", "red");
+axisY.setAttribute("stroke-width", "3");
+svg.appendChild(axisY);
+
+document.body.appendChild(svg);
+
+function pick(e) {
+    if (e === undefined) return;
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const hue = Math.round((mouseX / window.innerWidth) * 360);
+    const luminosity = Math.round((mouseY / window.innerHeight) * 100);
+    const hsl = `hsl(${hue}, 100%, ${luminosity}%)`;
+    document.body.style.background = hsl;
+    hslDiv.innerHTML = hsl;
+    hueDiv.innerHTML = `${hue}`;
+    luminosityDiv.innerHTML = `${luminosity}`;
+    drawLines(mouseX, mouseY);
+}
+
+function drawLines(x, y) {
+    axisX.setAttribute("x1", x);
+    axisX.setAttribute("x2", x);
+    axisY.setAttribute("y1", y);
+    axisY.setAttribute("y2", y);
+}
+
+async function copyHSL() {
+    try {
+        await navigator.clipboard.writeText(hslDiv.innerHTML);
+    } catch (err) {
+        console.error("Failed to copy: ", err);
+    }
+}
+
+export { pick };
