38bd6b36d346b403f6a844b67f7b655863dda59d
sahmed <engsameergoumaa@gmail.com>
2023-12-11 13:05:37 +0300

    obj-12

--- flagger.js
+++ flagger.js
@@ -1,33 +1,26 @@
 function flags(obj) {
     var res = { alias: { h: 'help' } };
-  
-    if (Object.keys(obj).length === 0) return res;
-  
+    if (obj.length === 0) return res;
     let help = obj.help;
     delete obj.help;
-  
     for (let key in obj) {
-      res.alias[key[0]] = key;
+        res.alias[key[0]] = key;
     }
-  
     if (help) {
-      res.description = help.map((key) => {
-        let desc = obj[key];
-        return `-${key[0]}, --${key}: ${desc}`;
-      });
+        res.description = help.map((key) => {
+            let desc = obj[key];
+            return `-${key[0]}, --${key}: ${desc}`;
+        });
     } else {
-      res.description = Object.keys(obj).map((key) => {
-        let desc = obj[key];
-        return `-${key[0]}, --${key}: ${desc}`;
-      });
+        res.description = Object.keys(obj).map((key) => {
+            let desc = obj[key];
+            return `-${key[0]}, --${key}: ${desc}`;
+        });
     }
-  
-    res.description =
-      res.description && res.description.length > 0
-        ? Array.isArray(res.description)
-          ? res.description.join('\n')
-          : res.description
-        : '';
-  
+    res.description.length === 0
+        ? (res.description = '')
+        : res.description.length === 1
+        ? (res.description = res.description[0])
+        : (res.description = res.description.join('\n'));
     return res;
-  }
+}
