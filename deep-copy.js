0160d1d9d7b2ac2ab0eeda3d9042d5d4d399425a
sahmed <engsameergoumaa@gmail.com>
2023-12-11 09:51:21 +0300

    obj-8

--- deep-copy.js
+++ deep-copy.js
@@ -1,18 +1,27 @@
-function deepCopy(obj) {
-    if (obj === null || typeof obj !== 'object') {
-      return obj;
+function deepCopy(input) {
+    if (Array.isArray(input)) {
+        var output = [];
+        for (var i = 0; i < input.length; i++) {
+            output[i] = deepCopy(input[i]);
+        }
+        return output;
+    } else if (isDefenitelyAnObject(input)) {
+        var output = {};
+        for (var key in input) {
+            output[key] = deepCopy(input[key]);
+        }
+        return output;
+    } else {
+        return input;
     }
-  
-    if (Array.isArray(obj)) {
-      return obj.map((item) => deepCopy(item));
-    }
-  
-    const copiedObj = {};
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        copiedObj[key] = deepCopy(obj[key]);
-      }
-    }
-  
-    return copiedObj;
-  }
+}
+
+function isDefenitelyAnObject(input) {
+    return (
+        typeof input === "object" &&
+        !(typeof input === "function") &&
+        !Array.isArray(input) &&
+        input !== null &&
+        !(input instanceof RegExp)
+    );
+}
