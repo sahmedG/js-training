4b547afdc064f00b11e2048fcab1c213c71df282
sahmed <engsameergoumaa@gmail.com>
2023-12-11 11:02:55 +0300

    obj-10

--- pronoun.js
+++ pronoun.js
@@ -9,10 +9,9 @@ function pronoun(str) {
         acc[pronoun] = {
           word: matches.map((match) => {
             const index = str.indexOf(match) + match.length + 1;
-            const nextWord = str
-              .slice(index, str.indexOf(' ', index) !== -1 ? str.indexOf(' ', index) : undefined)
-              .replace(/[\n\r]+/g, '')
-              .split(',')[0];
+            const nextSpace = str.indexOf(' ', index);
+            const endIndex = nextSpace !== -1 ? nextSpace : undefined;
+            const nextWord = str.slice(index, endIndex).replace(/[\n\r]+/g, '').split(',')[0];
             return nextWord;
           }),
           count: matches.length,
