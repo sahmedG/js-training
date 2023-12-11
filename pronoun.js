beeb523831e3c61c2a5412b5f08a6165e23e9fb7
sahmed <engsameergoumaa@gmail.com>
2023-12-11 10:27:58 +0300

    obj-10

---
+++ pronoun.js
@@ -0,0 +1,23 @@
+function pronoun(str) {
+    const pronouns = ['i', 'you', 'he', 'she', 'it', 'they', 'we'];
+  
+    const pronounOccurrences = pronouns.reduce((acc, pronoun) => {
+      const regex = new RegExp(`\\b${pronoun}\\b`, 'gi');
+      const matches = str.match(regex);
+  
+      if (matches) {
+        acc[pronoun] = {
+          word: matches.map((match) => {
+            const index = str.indexOf(match) + match.length + 1;
+            const nextWord = str.slice(index, str.indexOf(' ', index) !== -1 ? str.indexOf(' ', index) : undefined);
+            return nextWord;
+          }),
+          count: matches.length,
+        };
+      }
+  
+      return acc;
+    }, {});
+  
+    return pronounOccurrences;
+  }
