d28957b16aa2e49eb9dc57bc06c843f6df0eccfb
sahmed <engsameergoumaa@gmail.com>
2023-12-12 13:16:59 +0300

    async-05

--- get-json.js
+++ get-json.js
@@ -1,25 +1,25 @@
-function getJSON(path, params) {
-    const url = new URL(path);
-    if (params) {
-      Object.keys(params).forEach((key) => url.searchParams.append(key, params[key]));
-    }
-  
-    return fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-  
-        return response.json();
-      })
-      .then((result) => {
-        if (result && result.hasOwnProperty('data')) {
-          return result.data;
-        } else if (result && result.hasOwnProperty('error')) {
-          throw new Error(result.error);
+async function getJSON(path = '', params = {}) {
+    const url =
+        path +
+        '?' +
+        Object.keys(params)
+            .map((key) => {
+                return (
+                    key.replace(' ', '+') +
+                    '=' +
+                    params[key].toString().replace(' ', '+')
+                );
+            })
+            .join('&');
+    const res = await fetch(url).then((response) => {
+        if (response.ok) {
+            return response.json();
         } else {
-          throw new Error('Invalid response format');
+            throw new Error(response.statusText);
         }
-      });
-  }
-  
+    });
+    if (res.error) {
+        throw new Error(res.error);
+    }
+    return res.data;
+}
