0e7b66358ebf325cc7d74537b85f7dc8c294efe4
sahmed <engsameergoumaa@gmail.com>
2023-12-12 12:49:40 +0300

    async-03

--- interpolation.js
+++ interpolation.js
@@ -1,20 +1,20 @@
-function interpolation({ step, start, end, callback, duration }) {
-    const totalPoints = step + 1;
-    const distanceInterval = (end - start) / step;
-    const timeInterval = duration / step;
-  
-    function interpolatePoints() {
-      let currentDistance = start;
-  
-      for (let i = 0; i < step; i++) {
-        const currentPoint = [currentDistance, callback(currentDistance)];
-        setTimeout(() => {
-          callback(currentPoint);
-        }, i * timeInterval);
-  
-        currentDistance += distanceInterval;
-      }
-    }
-  
-    interpolatePoints();
-  }
+function interpolation({
+    step = 0,
+    start = 0,
+    end = 0,
+    callback = () => {},
+    duration = 0,
+} = {}) {
+    const delta = (end - start) / step;
+    let current = start;
+    let i = 0;
+    const timer = setInterval(() => {
+        if (i < step) {
+            callback([current, (duration / step) * (i + 1)]);
+            current += delta;
+            i++;
+        } else {
+            clearInterval(timer);
+        }
+    }, duration / step);
+}
