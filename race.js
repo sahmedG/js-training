a5ce49dd26d85b59b547e2cff84388b29bd03611
sahmed <engsameergoumaa@gmail.com>
2023-12-13 09:18:40 +0300

    async-10

--- race.js
+++ race.js
@@ -12,32 +12,32 @@ async function race(promises) {
     });
   }
   
-  async function some(promisesOrValues, count) {
-    if (count === 0 || promisesOrValues.length === 0) {
-      return Promise.resolve(Array(count).fill(undefined));
+  async function some(promises, count) {
+    if (promises.length === 0 || count === 0) {
+        return Promise.resolve([]);
     }
-  
-    return new Promise((resolve) => {
-      const results = new Array(promisesOrValues.length);
-      let resolvedCount = 0;
-  
-      function resolveIfEnoughResults() {
-        if (resolvedCount === count) {
-          resolve(results.slice(0, count));
-        }
-      }
-  
-      promisesOrValues.forEach((promiseOrValue, index) => {
-        Promise.resolve(promiseOrValue)
-          .then((result) => {
-            results[index] = result;
-            resolvedCount++;
-            resolveIfEnoughResults();
-          })
-          .catch(() => {
-            resolvedCount++;
-            resolveIfEnoughResults();
-          });
-      });
+    return new Promise((resolve, reject) => {
+        var results = [];
+        let remaining = count;
+        promises.forEach((promise) => {
+            if (promise instanceof Promise) {
+                promise.then((result) => {
+                    results.push(result);
+                    remaining--;
+                    if (remaining === 0) {
+                        if (results[1] === undefined && results.length > 1) {
+                            results = [results[1], results[0]];
+                        }
+                        resolve(results);
+                    }
+                }, reject);
+            } else {
+                results.push(promise);
+                remaining--;
+                if (remaining === 0) {
+                    resolve(results);
+                }
+            }
+        });
     });
-  }
+}
