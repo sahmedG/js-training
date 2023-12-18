ec24980d538c88447d82444a504f8928a62a0db6
sahmed <engsameergoumaa@gmail.com>
2023-12-18 14:56:34 +0300

    node-03

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -2,6 +2,7 @@
 
 import { readFileSync } from 'fs';
 
+// Function to reverse a very disco word
 function reverseVeryDisco(word) {
   const length = Math.ceil(word.length / 2);
   const firstHalf = word.slice(0, length);
@@ -9,13 +10,16 @@ function reverseVeryDisco(word) {
   return secondHalf + firstHalf;
 }
 
+// Process command line arguments
 const fileName = process.argv[2];
 
+// Check if a file name is provided
 if (!fileName) {
   console.error('Please provide a file name.');
   process.exit(1);
 }
 
+// Read the content of the file
 let fileContent;
 try {
   fileContent = readFileSync(fileName, 'utf8');
@@ -24,8 +28,11 @@ try {
   process.exit(1);
 }
 
+// Split the content into words
 const words = fileContent.split(' ');
 
+// Reverse each very disco word
 const reversedWords = words.map(reverseVeryDisco);
 
-console.log(reversedWords.join(''));
+// Display the result
+console.log(reversedWords.join(' '));
