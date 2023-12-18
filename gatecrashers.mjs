75addb0cd2ae6ae9e0dbe04e51ae126b09d7731c
sahmed <engsameergoumaa@gmail.com>
2023-12-19 01:27:18 +0300

    node-10

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -1,11 +1,54 @@
 import http from "http";
 import { promises as fs } from "fs";
 
-// Utility functions
 
-// ... (same as before)
-
-// List of authorized users and their passwords
+async function readFileToString(filePath) {
+    try {
+      // Check if the file exists
+      await fs.access(filePath);
+  
+      // If the file exists, read its contents
+      const fileContent = await fs.readFile(filePath, "utf-8");
+      return fileContent;
+    } catch (error) {
+      // Handle errors, e.g., file not found
+      if (error.code === "ENOENT") {
+        return 404;
+      } else {
+        console.error(`Error reading file: ${error.message}`);
+        return 500;
+      }
+    }
+  }
+  
+  async function writeFile(fileName, content) {
+    try {
+      await fs.writeFile(fileName, content, "utf8");
+      console.log(`File ${fileName} created successfully.`);
+      return 201;
+    } catch (err) {
+      console.error(`Error writing to ${fileName}: ${err}`);
+      return 500;
+    }
+  }
+  
+  // HANDLE GETS LIKE USUAL
+  const handleGET = async (req, res) => {
+    let jsonInfo = await readFileToString(`guests/${req.url}.json`);
+    if (jsonInfo === 404) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      const errorResponse = { error: "guest not found" };
+      res.end(JSON.stringify(errorResponse));
+    } else if (jsonInfo === 500) {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      const errorResponse = { error: "server failed" };
+      res.end(JSON.stringify(errorResponse));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(jsonInfo);
+    }
+  };
+  
 const authorizedUsers = {
   "Caleb_Squires": "abracadabra",
   "Tyrique_Dalton": "abracadabra",
