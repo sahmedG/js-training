8d06b83f921e691be03b2da55f0cc5fc79335adf
sahmed <engsameergoumaa@gmail.com>
2023-12-18 16:14:08 +0300

    node-07

--- tell-it-cypher.mjs
+++ tell-it-cypher.mjs
@@ -17,6 +17,7 @@ if (!keyword || (keyword !== 'encode' && keyword !== 'decode')) {
   process.exit(1);
 }
 
+// Optional third argument for the new file name
 const newFileName = process.argv[4] || 'output.txt';
 
 const fileContent = readFileSync(filePath, 'utf8');
@@ -24,8 +25,10 @@ const fileContent = readFileSync(filePath, 'utf8');
 let result;
 
 if (keyword === 'encode') {
+  // Encode to base64
   result = Buffer.from(fileContent, 'utf8').toString('base64');
 } else {
+  // Decode from base64
   try {
     result = Buffer.from(fileContent, 'base64').toString('utf8');
   } catch (error) {
@@ -34,6 +37,7 @@ if (keyword === 'encode') {
   }
 }
 
+// Save the result to the specified file in the current working directory
 const outputPath = resolve(process.cwd(), newFileName);
 
 try {
