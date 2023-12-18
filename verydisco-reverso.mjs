a4990f0779168fdf20276bb415818eb9ba196391
sahmed <engsameergoumaa@gmail.com>
2023-12-18 14:22:59 +0300

    node-03

---
+++ verydisco-reverso.mjs
@@ -0,0 +1,31 @@
+// verydisco-reverso.mjs
+
+import { readFileSync } from 'fs';
+
+function reverseVeryDisco(word) {
+  const length = Math.ceil(word.length / 2);
+  const firstHalf = word.slice(0, length);
+  const secondHalf = word.slice(length);
+  return secondHalf + firstHalf;
+}
+
+const fileName = process.argv[2];
+
+if (!fileName) {
+  console.error('Please provide a file name.');
+  process.exit(1);
+}
+
+let fileContent;
+try {
+  fileContent = readFileSync(fileName, 'utf8');
+} catch (error) {
+  console.error(`Error reading file: ${error.message}`);
+  process.exit(1);
+}
+
+const words = fileContent.split(' ');
+
+const reversedWords = words.map(reverseVeryDisco);
+
+console.log(reversedWords.join(' '));
