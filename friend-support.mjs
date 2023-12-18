527d28a13612f6ba081d2fdf8df6dbbb9b2dd160
sahmed <engsameergoumaa@gmail.com>
2023-12-18 23:35:54 +0300

    node-08

--- friend-support.mjs
+++ friend-support.mjs
@@ -1,83 +1,51 @@
-// // friend-support.mjs
+import http from "http";
+import { promises as fs } from "fs";
 
-// import http from 'http';
-// import fs from 'fs/promises';
-// import url from 'url';
+async function readFileToString(filePath) {
+  try {
+    // Check if the file exists
+    await fs.access(filePath);
 
-// const port = 5000;
-
-// const server = http.createServer(async (req, res) => {
-//   const guestName = req.url.substring(req.url.lastIndexOf('/') + 1).replace(/[^\w]/g, '_');
-//   let guestNumber;
-
-//   const urlParts = url.parse(req.url, true);
-//   const expBody = urlParts.query.expBody;
-
-//   if (guestName.includes('mario')) {
-//     const guestName2 = req.url.match(/mario_(\w+)$/);
-//     guestNumber = guestName2[1];
-//   }
-
-//   if (guestName === 'mario_0') {
-//     res.writeHead(500, { 'Content-Type': 'application/json' });
-//     res.end(JSON.stringify({ status: 500, body: { error: 'server failed' }, contentType: 'application/json' }));
-//     return;
-//   } else if (guestNumber !== 0 && expBody) {
-//     res.writeHead(200, { 'Content-Type': 'application/json' });
-//     res.end(JSON.stringify({ status: 200, body: { message: guestNumber }, contentType: 'application/json' }));
-//     return;
-//   } else  {
-//     res.writeHead(404, { 'Content-Type': 'application/json' });
-//     res.end(JSON.stringify({ status: 404, body: { error: 'guest not found' }, contentType: 'application/json' }));
-//     return;
-//   }
-// });
-
-// server.listen(port, () => {
-//   console.log(`Server listening on port ${port}`);
-// });
-// //if (guestName === 'andrea_bianchi')
-
-
-// friend-support.mjs
-
-import http from 'http';
-import fs from 'fs/promises';
-import url from 'url';
-const PORT = 5000;
+    // If the file exists, read its contents
+    const fileContent = await fs.readFile(filePath, "utf-8");
+    return fileContent;
+  } catch (error) {
+    // Handle errors, e.g., file not found
+    if (error.code === "ENOENT") {
+      return 404;
+    } else {
+      console.error('Error reading file: ${error.message}');
+      return 500;
+    }
+  }
+}
 
 const server = http.createServer(async (req, res) => {
-  // Extract guest name from the request URL
-  const guestName = req.url.slice(1); // Remove leading '/'
-  const filePath = `${guestName}.json`;
-  const urlParts = url.parse(req.url, true);
-  const expBody = urlParts.query.expBody;
-
-  try {
-    // Try to read the guest JSON file
-    const data = await fs.readFile(filePath, 'utf-8');
-    const guestInfo = JSON.parse(data);
-
-    // Respond with 200 OK and guest information
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(guestInfo));
-  } catch (error) {
-    if (expBody != "") {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: expBody }));
-    } else if (error.code === 'ENOENT') {
-      // Guest not found
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'guest not found' }));
+  if (req.method !== "GET") {
+    res.writeHead(405, { "Content-Type": "application/json" });
+    const errorResponse = { error: "Method Not Allowed" };
+    res.end(JSON.stringify(errorResponse));
+  } else {
+    let jsonInfo = await readFileToString('guests/${req.url}.json');
+    if (jsonInfo === 404) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      const errorResponse = { error: "guest not found" };
+      res.end(JSON.stringify(errorResponse));
+    } else if (jsonInfo === 500) {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      const errorResponse = { error: "server failed" };
+      res.end(JSON.stringify(errorResponse));
     } else {
-      // Server failed for some reason
-      res.writeHead(500, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'server failed' }));
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(jsonInfo);
     }
   }
 });
 
-// Listen for server start
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+const PORT = 5000;
+server.listen(PORT, (err) => {
+  if (err) {
+    return console.error('Error starting server: ${err}');
+  }
+  console.log('Server is listening on port ${PORT}');
 });
