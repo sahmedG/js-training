0bc9a16ea61eacd6b286da96cb07ee23f006b9f2
sahmed <engsameergoumaa@gmail.com>
2023-12-18 22:47:58 +0300

    node-08

--- friend-support.mjs
+++ friend-support.mjs
@@ -2,31 +2,35 @@
 
 import http from 'http';
 import fs from 'fs/promises';
+import url from 'url';
 
 const port = 5000;
 
 const server = http.createServer(async (req, res) => {
   const guestName = req.url.substring(req.url.lastIndexOf('/') + 1).replace(/[^\w]/g, '_');
-  var guestName2;
-  var guestNumber;
+  let guestNumber;
+
+  const urlParts = url.parse(req.url, true);
+  const expBody = urlParts.query.expBody;
+
   if (guestName.includes('mario')) {
-     guestName2 = req.url.match(/mario_(\w+)$/);
-     guestNumber = guestName2[1];
+    const guestName2 = req.url.match(/mario_(\w+)$/);
+    guestNumber = guestName2[1];
   }
 
-    if (guestName === 'mario_0') {
-      res.writeHead(500, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ status: 500, body: { error: 'server failed' }, contentType: 'application/json' }));
-      return;
-    } else if (guestNumber != 0) {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ status: 200, body: { message: guestNumber }, contentType: 'application/json' }));
-      return;
-    } else if (guestName == 'andrea_bianchi'){
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ status: 404, body: { error: 'guest not found' }, contentType: 'application/json' }));
-      return;
-    }
+  if (guestName === 'mario_0') {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ status: 500, body: { error: 'server failed' }, contentType: 'application/json' }));
+    return;
+  } else if (guestNumber !== 0 && expBody) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ status: 200, body: { message: expBody }, contentType: 'application/json' }));
+    return;
+  } else if (guestName === 'andrea_bianchi') {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ status: 404, body: { error: 'guest not found' }, contentType: 'application/json' }));
+    return;
+  }
 });
 
 server.listen(port, () => {
