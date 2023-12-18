35951378dd9c5cc5513d37bfdc2be908781fb410
sahmed <engsameergoumaa@gmail.com>
2023-12-18 20:44:30 +0300

    node-08

--- friend-support.mjs
+++ friend-support.mjs
@@ -1,49 +1,36 @@
 // friend-support.mjs
 
 import http from 'http';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import fs from 'fs/promises';
 
-const PORT = 5000;
+const port = 5000;
 
-const server = http.createServer((req, res) => {
-  // Check if the request is for the root endpoint
-  if (req.url === '/') {
+const server = http.createServer(async (req, res) => {
+  console.log(`Server listening on port ${port}`);
+
+  // Extract guest name from the request URL
+  const guestName = req.url.substring(1);
+
+  try {
+    // Read the guest JSON file
+    const data = await fs.readFile(`${guestName}.json`, 'utf-8');
+    const guestInfo = JSON.parse(data);
+
+    // Send a successful response with guest information
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Welcome to the friend-support server!' }));
-    return;
-  }
-
-  const guestName = req.url.substring(1); // Remove the leading slash
-
-  // Check if the request is a valid GET request
-  if (req.method === 'GET') {
-    try {
-      const guestFilePath = resolve(__dirname, `${guestName}.json`);
-      const guestData = readFileSync(guestFilePath, 'utf8');
-      const guestJson = JSON.parse(guestData);
-
-      // Send a successful response with the guest information
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(guestJson));
-    } catch (error) {
-      // Handle file not found
-      if (error.code === 'ENOENT') {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'guest not found' }));
-      } else {
-        // Handle other errors
-        res.writeHead(500, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'server failed' }));
-      }
+    res.end(JSON.stringify(guestInfo));
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      // Guest not found
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: 'guest not found' }));
+    } else {
+      // Server failed
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: 'server failed' }));
     }
-  } else {
-    // Respond with a 404 for non-GET requests
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ error: 'not found' }));
   }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+// Start the server
+server.listen(port);
