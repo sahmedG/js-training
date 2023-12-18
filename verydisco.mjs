9240cdaf70e4651e1a3d4ec51edbda97db7f1809
sahmed <engsameergoumaa@gmail.com>
2023-12-18 14:00:31 +0300

    node-01

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,43 +1,24 @@
-#!/usr/bin/env node
-
-import { argv } from 'process';
-
-function discoTransform(word) {
-    const length = word.length;
-    const halfLength = Math.ceil(length / 2);
-    const firstHalf = word.slice(0, halfLength);
-    const secondHalf = word.slice(halfLength);
-    const result = secondHalf + firstHalf;
-    return result;
-}
-
-function veryDisco(sentence) {
-    return sentence
-        .split(' ')
-        .map(word => 'verydisco')
-        .join(' ');
-}
-
-function main() {
-    // Check if there is at least one command line argument
-    if (argv.length < 3) {
-        console.log("Please provide a word or sentence as a command line argument.");
-        return;
-    }
-
-    // Get the first command line argument
-    const inputArgument = argv[2];
-
-    // If the argument contains spaces, treat each word as "very disco"
-    if (inputArgument.includes(' ')) {
-        const result = veryDisco(inputArgument);
-        console.log(result);
-    } else {
-        // Perform disco transformation on the single word
-        const result = discoTransform(inputArgument);
-        console.log(result);
-    }
-}
-
-// Run the script
-main();
+function makeVeryDisco(word) {
+    const length = Math.ceil(word.length / 2);
+    const firstHalf = word.slice(0, length);
+    const secondHalf = word.slice(length);
+    return secondHalf + firstHalf;
+  }
+  
+  // Process command line arguments
+  const inputArg = process.argv[2];
+  
+  // Check if an argument is provided
+  if (!inputArg) {
+    console.error('Please provide an argument.');
+    process.exit(1);
+  }
+  
+  // Split the input into words
+  const words = inputArg.split(' ');
+  
+  // Make each word very disco
+  const veryDiscoWords = words.map(makeVeryDisco);
+  
+  // Display the result
+  console.log(veryDiscoWords.join(' '));
